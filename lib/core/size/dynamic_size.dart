12b9b423d2580b20aafeb8451218d0d9ff254873
MOHAMMED SALIQ KT <mohammed.saliq@akiraplc.com>
2024-09-12 21:29:38 +0530

    adf

--- lib/core/size/dynamic_size.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension DynamicSize on BuildContext {
-  double width(double width) => MediaQuery.sizeOf(this).width * width;
-  double height(double height) => MediaQuery.sizeOf(this).height * height;
-}
